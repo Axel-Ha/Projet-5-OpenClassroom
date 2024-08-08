01fdba7c9870f195e0c5fd4a0647c2aa2f2e0062
Axel Halifa <axel.halifa0@gmail.com>
2024-08-08 11:46:13 +0200

    Modification : taux de covering a 80%

---
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.starterjwt.services;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SessionService {
+
+}
